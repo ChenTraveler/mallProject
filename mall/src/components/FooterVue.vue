6945e3c544532b85629f349187a4a9bf9a7bbe45
脑阔疼 <11764190+z2848046326@user.noreply.gitee.com>
2022-11-07 14:45:15 +0800

    zhou

---
+++ mall/src/components/FooterVue.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MallProjectFooterVue',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
