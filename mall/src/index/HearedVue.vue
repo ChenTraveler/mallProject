68bbbb60645ee374ddf56058de49d8611fb329df
ywq6688 <11764202+ywq6688@user.noreply.gitee.com>
2022-11-07 14:34:42 +0800

    ywq

---
+++ mall/src/index/HearedVue.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img src="../../public/images/index/a1.jpg"
+         alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  created() {},
+  mounted() {},
+}
+</script>
+
+<style lang=stylus scoped></style>
