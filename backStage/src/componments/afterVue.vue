f70667cd34e0ac0ac7276dc86ee659478bfe88fe
gc <1140265677@qq.com>
2022-11-07 14:19:06 +0800

    gc

---
+++ backStage/src/componments/afterVue.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>我是后台管理系统</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+
+<style lang=stylus>
+</style>
